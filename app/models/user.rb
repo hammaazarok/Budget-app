1027f4b1526a9737fdeb4f524e12195c5e54db8c
Hamma Azarok <hammaagazarok@gmail.com>
2022-11-15 10:32:55 +0200

    fix rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable
+    has_many :groups
+    has_many :payments
+
+    validates :name, presence: true , length: { minimum: 3 }
+    validates :email, presence: true, uniqueness: true , length: { minimum: 3 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password, presence: true , length: { minimum: 6 }
 end
