bbc044427b511f131432566abf40d23c8babe869
Hamma Azarok <hammaagazarok@gmail.com>
2022-11-15 10:36:13 +0200

    fix rubocop

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
-    belongs_to :user
-    has_and_belongs_to_many :payments
-    
-    validates :name, presence: true , length: { minimum: 3 }
-    validates :user_id, presence: true , numericality: { greater_than: 0 }
+  belongs_to :user
+  has_and_belongs_to_many :payments
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :user_id, presence: true, numericality: { greater_than: 0 }
 end
